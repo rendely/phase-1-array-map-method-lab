707852c015940fa9bfebffffba368eb1a0687f99
matthew rendely <matthew.rendely@gmail.com>
2023-03-16 20:34:20 -0700

    done

--- index.js
+++ index.js
@@ -12,5 +12,26 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  //Loop through each title in the tutorials and run title case capitalization
+  return tutorials.map(titleCase)
+}
+
+function titleCase(title){
+  //Split into words separated by space
+  const words = title.split(' ');
+
+  //Loop through each and capitalize word
+  const titleCasedWords = words.map(capitalizeWord);
+
+  //Rejoin words into single string and return it
+  return titleCasedWords.join(' ');
+}
+
+function capitalizeWord(word){
+//For each word capitalize first letter and join to rest of word
+const capitalizedWord = (
+  word.slice(0,1).toUpperCase() +
+  word.slice(1)
+) 
+return capitalizedWord
 }
